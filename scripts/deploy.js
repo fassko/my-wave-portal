9a84f8e50240c26395f147affcb0fca8119bf47a
Kristaps Grinbergs <fassko@gmail.com>
2021-12-20 21:26:37 +0200

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.001'),
+  });
+
+  await waveContract.deployed();
+
+  console.log('WavePortal address: ', waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
+
+//0x2f78eEB2189f2394270d97f45b4fd54e4F047175
